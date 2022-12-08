03548ca029c789681f976c660b54e379be727e41
ChunAn Wu <an.wu@canonical.com>
2022-12-08 13:28:56 +0800

    Add readme file
    impact:
        README.d
    
    description:
        add using guide
    
    test:
        N/A
    
    Signed-off-by: ChunAn Wu <an.wu@canonical.com>

---
+++ README.d
@@ -0,0 +1,19 @@
+environment
+1. ubuntu os
+2. python3
+    a. pyserail lib
+    a. schedule lib
+3. git
+
+
+command guide
+
+PERIODIC:
+    hour example:
+        PERIODIC hour
+    daily example:
+        PERIODIC day ["tt:mm"]
+    weakly example:
+        PERIODIC week day(mon/tue/wed/thu/fri/sat/sun) ["tt:mm"]
+
+
