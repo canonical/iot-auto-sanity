53a57e69ffaadec2ac474b287a901c3fb76b1989
ChunAn Wu <an.wu@canonical.com>
2022-12-13 16:07:05 +0800

    update mailing list again
    impact:
        auto-sanity.py
    
    description:
        Add team members, we suffer together.
    
    test:
       N/A
    
    Signed-off-by: ChunAn Wu <an.wu@canonical.com>

--- auto-sanity.py
+++ auto-sanity.py
@@ -197,7 +197,7 @@ FAILED = 1
 PASSWD = "ectgbttmpfsbxxrg"
 SSHPWD = "passwd"
 fromaddr = "an.wu@canonical.com"
-recipients = ["oem-sanity@lists.canonical.com", "an.wu@canonical.com"]
+recipients = ["oem-sanity@lists.canonical.com", "rex.tsai@canonical.com", "robert.liu@canonical.com", "ethan.hsieh@canonical.com", "soar.huang@canonical.com", "aristo.chen@canonical.com", "laider.lai@canonical.com", "an.wu@canonical.com"]
 WORK_FLAG = False
 columns = shutil.get_terminal_size().columns
 
