51eef53a731a3ac373b0f43e800fbc71b4d8c40c
ChunAn Wu <an.wu@canonical.com>
2023-08-10 15:50:33 +0800

    Remove -f option during remove and copy seed folder
    impact:
        auto-sanity.py
    
    description:
        Remove -f option then we can know the detail of remove and copy seed files
    
    test:
        N/A
    
    Signed-off-by: ChunAn Wu <an.wu@canonical.com>

--- auto-sanity.py
+++ auto-sanity.py
@@ -74,8 +74,8 @@ def deploy(method,user_init ,timeout=600):
                 return FAILED
 
             write_con('cd ~/')
-            write_con('sudo rm -fr /run/mnt/ubuntu-seed/* ')
-            write_con('sudo cp -fr seed/* /run/mnt/ubuntu-seed/')
+            write_con('sudo rm -r /run/mnt/ubuntu-seed/* ')
+            write_con('sudo cp -r seed/* /run/mnt/ubuntu-seed/')
             # We don't wait for prompt due to system could possible reboot immediately without prompt
             write_con_no_wait('sudo snap reboot --install')
             write_con_no_wait('sudo reboot')
