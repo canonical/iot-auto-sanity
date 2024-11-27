3fbc9d053811b1c5d9e1ca8f87e7350b15053722
Laider Lai <laider.lai@canonical.com>
2024-11-27 07:53:06 +0000

    Support an IoT deployment method with a USB stick that flashed with an EFI based ISO  - The EFI based ISO auto-install target image to the target device  - Communicate with ISO installer and target device by SSH  - Attach an Iot-x86 Austin-SE30 tplan example - austin-se30.yaml
    Signed-off-by: Laider Lai <laider.lai@canonical.com>

--- sanity/agent/deploy.py
+++ sanity/agent/deploy.py
@@ -11,6 +11,7 @@ from sanity.agent.cmd import syscmd
 from sanity.agent.style import columns
 from sanity.agent.err import FAILED, SUCCESS
 from sanity.agent.data import DevData
+from sanity.agent.iso import AutoISO
 
 
 INSTALL_MODE = "install"
@@ -369,7 +370,8 @@ def deploy(
 
             if method == "seed_override_nocheck":
                 return {"code": SUCCESS}
-
+        case "iso":
+            return AutoISO().result(con.getname())
         case _:
             return {"code": FAILED}
 
